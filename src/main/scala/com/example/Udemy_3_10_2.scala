da674cff7d04f7b837c0fe9d90f47758af513002
Michal Huniewicz <michal.huniewicz@bglgroup.co.uk>
2019-09-15 18:35:19 +0100

    Use object class.

--- src/main/scala/com/example/Udemy_3_10_2.scala
+++ src/main/scala/com/example/Udemy_3_10_2.scala
@@ -1,18 +1,13 @@
 package com.example
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import com.typesafe.scalalogging.{Logger, StrictLogging}
+import com.typesafe.scalalogging.StrictLogging
 
 object Udemy_3_10_2 extends App with StrictLogging {
 
-  case class Deposit(amount: BigDecimal)
-  case class Withdraw(amount: BigDecimal)
-  case class Statement()
-
-  case class Success(message: String = "Success")
-  case class Failure(message: String)
-
   class BankAccount(private var currentAmount: BigDecimal) extends Actor {
+    import BankAccount.{Deposit, Failure, Statement, Success, Withdraw}
+
     override def receive: Receive = {
       case Deposit(amount) =>
         logger.info(s"Amount [$amount] is being deposited to [${self.path.name}].")
@@ -32,7 +27,7 @@ object Udemy_3_10_2 extends App with StrictLogging {
           logger.info(s"Returning failure to [${context.sender().path.name}].")
           context.sender() ! Failure("You do not have enough gold.")
         }
-      case Statement() =>
+      case Statement =>
         logger.info("Asking about amount.")
         context.sender() ! currentAmount
       case x =>
@@ -41,15 +36,25 @@ object Udemy_3_10_2 extends App with StrictLogging {
   }
 
   object BankAccount {
+
+    case class Deposit(amount: BigDecimal)
+    case class Withdraw(amount: BigDecimal)
+    case object Statement
+
+    case class Success(message: String = "Success")
+    case class Failure(message: String)
+
     def props(initialAmount: BigDecimal): Props = Props(new BankAccount(initialAmount))
   }
 
   class BankClient(private val bankAccount: ActorRef) extends Actor {
+    import BankAccount.{Deposit, Failure, Statement, Success, Withdraw}
+
     override def receive: Receive = {
       case deposit: Deposit => bankAccount ! deposit
       case withdraw: Withdraw => bankAccount ! withdraw
-      case statement: Statement =>
-        bankAccount ! statement
+      case Statement =>
+        bankAccount ! Statement
       case Success(message) => logger.info(s"Success: $message")
       case Failure(message) => logger.info(s"Fail: $message")
       case amount: BigDecimal => logger.info(s"$amount")
@@ -64,11 +69,12 @@ object Udemy_3_10_2 extends App with StrictLogging {
   val account = system.actorOf(BankAccount.props(1000), "BankAccount1")
   val client = system.actorOf(BankClient.props(account), "Client1")
 
-  client ! Statement()
+  import BankAccount.{Deposit, Statement, Withdraw}
+  client ! Statement
   client ! Deposit(90)
   client ! Deposit(9)
   client ! Withdraw(599)
   client ! Withdraw(600)
-  client ! Statement()
+  client ! Statement
 
 }
