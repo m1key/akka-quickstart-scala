382bb393076fd9ccd5b7ad5eecab989973c9c60b
Michal Huniewicz <michal.huniewicz@bglgroup.co.uk>
2019-11-20 12:47:54 +0000

    Added some notes on back pressure.

--- src/main/scala/com/streams/BackpressureBasics.scala
+++ src/main/scala/com/streams/BackpressureBasics.scala
@@ -4,6 +4,9 @@ import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 
+/**
+ * Back-pressure protocol is transparent to us, but we can control it.
+ */
 object BackpressureBasics extends App {
 
   implicit val system = ActorSystem("BackpressureBasics")
@@ -12,7 +15,7 @@ object BackpressureBasics extends App {
   val fastSource = Source(1 to 1000)
   val slowSink = Sink.foreach[Int] {
     x => // Simulate long processing.
-      Thread.sleep(1000)
+      Thread.sleep(500)
       println(s"Sink $x")
   }
 
@@ -25,11 +28,15 @@ object BackpressureBasics extends App {
       x + 1
   }
 
-//  fastSource.async.via(simpleFlow).async.to(slowSink).run()
+  // This is good to illustrate. Also, every component runs on a different actor.
+  // Simple flow uses its internal buffer. Its size is 16. After 16 elements received, it will send a back-pressure
+  // signal to the source.
+  // Then, after the sink processes some elements, the flow allows for more elements to come through.
+  fastSource.async.via(simpleFlow).async.to(slowSink).run()
 
   val bufferedFlow = simpleFlow.buffer(10, overflowStrategy = OverflowStrategy.dropHead)
 
 //  fastSource.async.via(bufferedFlow).async.to(slowSink).run()
-  import scala.concurrent.duration._
-  fastSource.throttle(2, 1 second).runWith(Sink.foreach(println))
+//  import scala.concurrent.duration._
+//  fastSource.throttle(2, 1 second).runWith(Sink.foreach(println))
 }
